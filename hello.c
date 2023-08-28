35fc374d213a7b35d40991b7182745d62cfa3ba1
Rylan Becker <rbecker12@huskers.unl.edu>
2023-08-28 15:20:35 -0500

    intial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Rylan Becker
+ *         rbecker12@huskers.unl.edu
+ *
+ * Date: 2023/08/24
+ *
+ * A simple name-and-major-displaying program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Rylan Becker/n");
+
+  return 0;
+}
